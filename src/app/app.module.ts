5b25fee7d46aec05879243ed857442a66c710d53
Juwana <juwanazerman@gmail.com>
2019-01-14 17:58:08 -0500

    Update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { AutofocusModule } from 'angular-autofocus-fix';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent,
     TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AutofocusModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
